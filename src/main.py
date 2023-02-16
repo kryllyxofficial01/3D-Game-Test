86b39dc0432e693a1cc9e1cc5ce0ca003f3b216c
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-02-15 21:20:15 -0500

    dunno

--- src/main.py
+++ src/main.py
@@ -2,13 +2,30 @@ import ursina
 from ursina.prefabs.first_person_controller import FirstPersonController
 from voxel import Voxel
 
+# Setup code
 game = ursina.Ursina()
 player = FirstPersonController()
+info = ursina.Text(
+    text = "Press ESC to exit the game...",
+    origin = (2.17,-19)
+)
 
-for z in range(20):
-    for x in range(20):
+# Simple configurations
+ursina.mouse.locked = True
+ursina.window.fullscreen = True
+ursina.window.title = "3D Game"
+
+# Generate a basic platform
+for x in range(8):
+    for z in range(8):
         voxel = Voxel(
             position = (x,0,z)
         )
 
+# Runs every tick
+def update():
+    if ursina.held_keys["escape"]:
+        game.userExit()
+
+# Start the engine
 game.run()
